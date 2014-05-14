790530eb1e3a540b8537303c6d5cd12e25257349
rjspencer <rjspencer82@gmail.com>
2014-05-14 01:02:36 +0000

    Pseudocode w4.1

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -11,8 +11,28 @@
 # What is the input?
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
+/*
+Create total() method.
+Input an array of numbers.
+  >Create a variable sum set to 0.
+  >Loop through the array.
+  >Add each array value  to sum variable
+Return a sum of the numbers in the array.
+  
+Create sentence_maker method.
+Input an array of words
+  >Create a variable named sentence set to ''.
+  >Loop through the array.
+  >Concatenate each value to sentence variable.
+  >Capitalize the first letter of sentence.
+  >Check for a period at the last value in sentence.
+      >Add period if no period exists.
+Return sentence.
 
-
+      Things to try humanize()
+    Capitalize the first array value, then loop value [2] through value[length-1], then add a period.
+        
+   */   
 # 2. Initial Solution
 
 
