88d01ccac98a869bcf13d14acd8db8aeba6d59bb
Ryan Spencer - K53T <rjspencer82@gmail.com>
2014-05-15 22:41:56 -0500

    Refactor and reflections

--- week_4/2_creative/create_accountability_groups/create_groups_spec.rb
+++ week_4/2_creative/create_accountability_groups/create_groups_spec.rb
@@ -2,17 +2,17 @@
 require_relative "my_solution" 
 
 # Test Data
-students = ['John Smith', 'Jane Doe', 'Henry Winkler', 'Carrie Fisher', 'Gerald Ford', 'Mitt Romney']
+students = ['John Smith', 'Jane Doe', 'Henry Winkler', 'Carrie Fisher', 'Gerald Ford', 'Mitt Romney', 'Ben Stiler', 'George Washington', 'John Smith', 'Jane Doe', 'Henry Winkler', 'Carrie Fisher', 'Gerald Ford', 'Mitt Romney', 'Ben Stiler', 'George Washington']
 
 describe "group_creator" do
    it "should return an array of arrays" do
-      group_creator(students).is_a?
+      group_creator(students).kind_of?(Array)
    end
    it "should return an array with at least ( number of students / 4 ) arrays" do
-      group_creator(students).length >= ( students / 4 )
+      group_creator(students).length >= ( students.length / 4 )
    end
    it "should return an array with no more than ( number of students / 4 + 1) arrays" do
-      group_creator(students).length <= ( students / 4 + 1 )
+      group_creator(students).length <= ( (students.length / 4) + 1 )
    end
    it "should have the same number of students in all arrays as were passed to the method"  do
       count=0
