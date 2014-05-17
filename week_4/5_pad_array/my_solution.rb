897e52d423e063bf73b81d20664d23a49e11fcfa
rjspencer <rjspencer82@gmail.com>
2014-05-17 01:11:17 +0000

    Pad created and refactored n2o

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -8,18 +8,58 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? Integer - New element count, Optional value to use as the pad
+# What is the output? (i.e. What should the code return?) Code should return a new array or !modify the array it is called on 
 # What are the steps needed to solve the problem?
+=begin
+If array length is less than new element count
+  Push pad onto end of array while array length < new element count
+return array
 
+If array length is less than new element count
+  Push pad onto end of !this array while array length < new element count
+End
+
+=end
 
 # 2. Initial Solution
+=begin
+class Array
+  def pad ( count , pad = nil )
+    temp_array = self.clone
+    while temp_array.length < count
+      temp_array << pad
+    end
+    return temp_array
+  end
 
-
-
-
+  def pad! ( count , pad = nil )
+    while self.length < count
+      self << pad
+    end
+    return self
+  end
+end
+=end
 # 3. Refactored Solution
 
+class Array
+  def pad ( count , pad = nil )
+    temp_array = self.clone
+    temp_array << pad while temp_array.length < count
+    return temp_array
+  end
 
+  def pad! ( count , pad = nil )
+    self << pad while self.length < count
+    return self
+  end
+end
 
 # 4. Reflection 
+=begin
+      I like the idea of extending a class.  It would  seem to enable even more natural type in Ruby.  However, I would fear that too much extension on a built in class could cause confusion for other programmers.  I think I read that we can make new classes that inherit the features of another class.  Then we can create our own documentation and there are no preconcieved notions of what it should do.
+
+
+          I like the readability of my while statements.
+=end
