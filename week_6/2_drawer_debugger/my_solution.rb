fc363922f9ae65dc661bece7382dfa1510ca03ed
Ryan Spencer - K53T <rjspencer82@gmail.com>
2014-05-28 06:09:53 -0500

    Not sure how to hendle thses bugs

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -7,63 +7,69 @@
 # 2. Original Code
 
 class Drawer
+	attr_reader :contents
 
-attr_reader :contents
+	# Are there any more methods needed in this class?
 
-# Are there any more methods needed in this class?
+	def initialize()
+		@contents = []
+		@open = true
+	end
 
-def initialize
-@contents = []
-@open = true
+	def open
+		@open = true
+	end
+
+	def close
+		@open = false
+	end 
+
+	def add_item(item)
+		@contents << item
+	end
+
+	def remove_item(item = @contents.pop) #what is `#pop` doing? Defaults to removing the last element of the array
+		@contents.delete(item)
+		return item
+	end
+
+	def dump  # what should this method return? 
+		@contents.each { |i| i.remove_item }
+		puts "Your drawer is empty."
+	end
+
+	def view_contents
+		puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+	attr_reader :type
 
-# Are there any more methods needed in this class?
+	# Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+	end
 
+	def clean_silverware
+		puts "cleaning the #{type}"
+		@clean = true
+	end
 end
 
 knife1 = Silverware.new("knife")
-
+puts knife1
 silverware_drawer = Drawer.new
-silverware_drawer.add_item(knife1) 
+puts silverware_drawer
+silverware_drawer.add_item(knife1)
 silverware_drawer.add_item(Silverware.new("spoon"))
 silverware_drawer.add_item(Silverware.new("fork")) 
 silverware_drawer.view_contents
@@ -71,7 +77,6 @@ silverware_drawer.view_contents
 silverware_drawer.remove_item
 silverware_drawer.view_contents
 sharp_knife = Silverware.new("sharp_knife")
-
 
 silverware_drawer.add_item(sharp_knife)
 
@@ -83,10 +88,11 @@ removed_knife.clean_silverware
 
 silverware_drawer.view_contents
 silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
+silverware_drawer.view_contents #What should this return? nothing
 
-
+p fork
 fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+p fork
 fork.eat
 
 #BONUS SECTION
